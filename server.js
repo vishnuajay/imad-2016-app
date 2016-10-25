c78642ccc2fb31acefb87d10981d5a55c50b9fc7
vishnuajay <vishnuajay12@gmail.com>
2016-10-25 11:51:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/article', function (req, res) {
+  res.sendFile('hoiiiii');
+});
 
 
 app.get('/ui/style.css', function (req, res) {
