15e506375c6b5fb6fdc297a6d3a0575b6165676d
vishnuajay <vishnuajay12@gmail.com>
2016-11-09 21:36:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ request.onreadystatechange=function(){
             names=JSON.parse(names);
             var list="";
             for(var i=0;i<names.length;i++){
-                list+='<li>'+names[i]+'</li>'+'</hr>';
+                list+='<tr><td>'+names[i]+'</td></tr>'+'</hr>';
             }
             var ul=document.getElementById('comment_list');
             ul.innerHTML=list;
