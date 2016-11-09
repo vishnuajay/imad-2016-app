86e428f816db9f4b30cde2a18369a2ecfb9a2c84
vishnuajay <vishnuajay12@gmail.com>
2016-11-09 19:25:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,24 @@
 console.log('Loaded!');
+var submit=document.getElementById('submit_btn');
+submit.onclick=function(){
+var request=XMLHttpRequest();
+request.onreadystatechange=function(){
+    if(request.readyState===XMLHttpRequest.DONE){
+        if(request.status===200){
+            var names=request.responseText;
+            names=JSON.parse(names);
+            var list="";
+            for(var i=0;i<names.length;i++){
+                list+='<li>'+names[i]+'</li>'
+            }
+            var ul=document.getElementById('comment_list');
+            ul.innerHTML=list;
+        }
+    }
+};
+var nameInput=document.getElementById('comment_box');
+var name=nameInput.value;
+request.open('GET','http://vishnuajay.imad.hasura-app.io/submit_name?name='+name,true);
+request.send(null);
+};
+    
