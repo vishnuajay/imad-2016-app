490f9bb91732250116538ceb190814270412ad14
vishnuajay <vishnuajay12@gmail.com>
2016-11-10 23:54:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,7 +85,18 @@ app.post('/login',function(req,res){
         } 
     });
 });
-app.get()
+app.get('/check-login',function(req,res){
+    if(req.session&& req.session.auth&& req.session.auth.userId){
+        res.send('you are logged in:'+req.session.auth.userId.toString());
+        
+    }else{
+        res.send('you are not logged in');
+    }
+});
+app.get('/logout',function(req,res){
+    delete req.session.auth;
+    res.send('logged out')l
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
