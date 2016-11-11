73cb4dba1ee979fb9f6c3ee3d83f72ba32919a00
vishnuajay <vishnuajay12@gmail.com>
2016-11-11 20:58:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -53,4 +53,30 @@ var username = document.getElementById('uname').value;
         
     };
     
+     var register = document.getElementById('reg_btn');
+    register.onclick = function () {
+        var request = new XMLHttpRequest();
+        
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  alert('User created successfully');
+                  register.value = 'Registered!';
+              } else {
+                  alert('Could not register the user');
+                  register.value = 'Register';
+              }
+          }
+        };
+        var username = document.getElementById('runame').value;
+        var password = document.getElementById('rpass').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', '/create-user', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        register.value = 'Registering...';
     
+    };
+
