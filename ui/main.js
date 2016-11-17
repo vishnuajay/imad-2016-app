9c9ed33d0f0845e0c76d3429006383fa43204510
vishnuajay <vishnuajay12@gmail.com>
2016-11-17 19:39:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -88,10 +88,12 @@ var username = document.getElementById('username').value;
         var password = document.getElementById('password').value;
         console.log(username);
         console.log(password);
+        if(document.getElementById('username').value!==""&&document.getElementById('password').value!==""){
         request.open('POST', '/create-user', true);
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify({username: username, password: password}));  
         register.value = 'Registering...';
+        }
     
     };
  }
